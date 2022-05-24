245c47657358c915e2c664f9497e52b7a5034ec0
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-24 21:07:37 +0200

    feat: beginning with framer motion

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { Product } from "../../types";
 import { getProductsFromApi } from "../../services/api";
+import { motion } from "framer-motion";
 import "./Home.scss";
 
 const Home = () => {
@@ -15,7 +16,27 @@ const Home = () => {
 
   return (
     <main>
-      <h1>Home</h1>
+      <div className="wrapper header">
+{/*         <motion.div
+        animate={{ x: 0 }}
+        transition={{ ease: "easeOut", duration: 2 }}>
+          <span className='row-message'>
+            We are specialised in setting up the foundation of your brand and
+            setting you up for success.
+          </span>
+        </motion.div>
+ */}
+        <motion.div
+          initial={{ opacity: 0, y: 250 }}
+          animate={{ opacity: 1, y: 100 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 1
+          }}
+        >
+          <img src="https://picsum.photos/200/300" alt="imagen-prueba" />
+        </motion.div>
+      </div>
 
       <div className="product-card wrapper">
         {products.length > 0 ? products.map(product => (
@@ -30,6 +51,8 @@ const Home = () => {
           <span>loading...</span>
         }
       </div>
+
+      
     </main>
   )
 }
