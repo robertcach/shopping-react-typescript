2cb2dbaef698b3f0718e0781ae61985ab4f28aec
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-25 18:17:16 +0200

    feat: animate header images

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { Product } from "../../types";
 import { getProductsFromApi } from "../../services/api";
-import { motion } from "framer-motion";
+import { motion, useViewportScroll } from "framer-motion";
 import "./Home.scss";
 
 const Home = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const { scrollYProgress } = useViewportScroll()
 
   useEffect(() => {
     getProductsFromApi()
@@ -20,46 +21,69 @@ const Home = () => {
         <div className="images">
           <motion.div
             className="images__div"
-            initial={{ opacity: 0, y: 250, x: 0 }}
-            animate={{ opacity: 1, y: 100, x: 0 }}
+            initial={{ opacity: 0, y: 200, x: 0 }}
+            animate={{ opacity: 1, y: 75, x: 0 }}
             transition={{
               ease: "easeInOut",
               duration: 1
             }}
           >
-            <img src="https://picsum.photos/id/237/300/300" alt="imagen-prueba" />
+            <img src="https://picsum.photos/id/237/275/400" alt="imagen-prueba" />
           </motion.div>
 
           <motion.div
             className="images__div"
-            initial={{ opacity: 0, y: 250, x: 800 }}
-            animate={{ opacity: 1, y: 100, x: 800 }}
+            initial={{ opacity: 0, y: 250, x: 825 }}
+            animate={{ opacity: 1, y: -300, x: 825 }}
             transition={{
               ease: "easeInOut",
               duration: 1,
               delay: 0.5
             }}
           >
-            <img src="https://picsum.photos/300/300" alt="imagen-prueba" />
+            <img src="https://picsum.photos/275/400" alt="imagen-prueba" />
           </motion.div>
 
 
           <motion.div
             className="images__div"
-            initial={{ opacity: 0, y: 250, x: 200 }}
-            animate={{ opacity: 1, y: -150, x: 200 }}
+            initial={{ opacity: 0, y: 250, x: 100 }}
+            animate={{ opacity: 1, y: -450, x: 100 }}
             transition={{
               ease: "easeInOut",
               duration: 1,
               delay: 1
             }}
           >
-            <img src="https://picsum.photos/700/450" alt="imagen-prueba" />
+            <img src="https://picsum.photos/900/600" alt="imagen-prueba" />
           </motion.div>
 
 
+          <motion.div
+            className="images__div"
+            initial={{ opacity: 0, y: 150, x: 700 }}
+            animate={{ opacity: 1, y: -500, x: 700 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 1,
+              delay: 0.5
+            }}
+          >
+            <img src="https://picsum.photos/350/250" alt="imagen-prueba" />
+          </motion.div>
 
-
+          <motion.div
+            className="images__div"
+            initial={{ opacity: 0, y: 150, x: 15 }}
+            animate={{ opacity: 1, y: -775, x: 15 }}
+            transition={{
+              ease: "easeInOut",
+              duration: 1,
+              delay: 0.5
+            }}
+          >
+            <img src="https://picsum.photos/350/250" alt="imagen-prueba" />
+          </motion.div>
 
         </div>
         
