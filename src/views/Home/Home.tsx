069861b40f7c98863457a74c622e8d0df1aa2ade
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-26 11:06:48 +0200

    feat: desing home banner

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { Product } from "../../types";
 import { getProductsFromApi } from "../../services/api";
-import { motion, useViewportScroll } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import "./Home.scss";
+
+
 
 const Home = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const { scrollYProgress } = useViewportScroll()
 
   useEffect(() => {
     getProductsFromApi()
@@ -17,80 +18,34 @@ const Home = () => {
 
   return (
     <main>
-      <div className="wrapper header">
-        <div className="images">
-          <motion.div
-            className="images__div"
-            initial={{ opacity: 0, y: 200, x: 0 }}
-            animate={{ opacity: 1, y: 75, x: 0 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1
-            }}
-          >
-            <img src="https://picsum.photos/id/237/275/400" alt="imagen-prueba" />
-          </motion.div>
-
-          <motion.div
-            className="images__div"
-            initial={{ opacity: 0, y: 250, x: 825 }}
-            animate={{ opacity: 1, y: -300, x: 825 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1,
-              delay: 0.5
-            }}
-          >
-            <img src="https://picsum.photos/275/400" alt="imagen-prueba" />
-          </motion.div>
-
-
-          <motion.div
-            className="images__div"
-            initial={{ opacity: 0, y: 250, x: 100 }}
-            animate={{ opacity: 1, y: -450, x: 100 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1,
-              delay: 1
-            }}
-          >
-            <img src="https://picsum.photos/900/600" alt="imagen-prueba" />
-          </motion.div>
-
-
-          <motion.div
-            className="images__div"
-            initial={{ opacity: 0, y: 150, x: 700 }}
-            animate={{ opacity: 1, y: -500, x: 700 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1,
-              delay: 0.5
-            }}
-          >
-            <img src="https://picsum.photos/350/250" alt="imagen-prueba" />
-          </motion.div>
-
-          <motion.div
-            className="images__div"
-            initial={{ opacity: 0, y: 150, x: 15 }}
-            animate={{ opacity: 1, y: -775, x: 15 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1,
-              delay: 0.5
-            }}
-          >
-            <img src="https://picsum.photos/350/250" alt="imagen-prueba" />
-          </motion.div>
-
+      <div className="header wrapper">
+        <div className="header__left">
+          <h1 className="header__title">New trendy Fashion</h1>
+          <button className="header__btn">Go to shop</button>
         </div>
-        
+
+        <motion.div
+          className="header__center"
+          initial={{ opacity: 0, y: 200, x: 50 }}
+          animate={{ opacity: 1, y: 0, x: 50 }}
+          transition={{
+            ease: "easeOut",
+            duration: 1
+          }}
+        >
+          <img src="https://picsum.photos/id/157/600/750" alt="imagen-superior-izquierda" />
+        </motion.div>
+
+        <div className="header__right">
+          <p className="header__subtitle">I must explains to you how all this idea of denouncing pleasure</p>
+        </div>  
+
       </div>
+        
+
 
       <div className="product-card wrapper">
-        {products.length > 0 ? products.map(product => (
+        {products.length > 0 && products.map(product => (
             <ProductCard 
               id={product.id}
               image={product.image}
@@ -98,8 +53,7 @@ const Home = () => {
               title={product.title}
               key={product.id}
             />
-          )) : 
-          <span>loading...</span>
+          ))
         }
       </div>
 
